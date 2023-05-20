1069c6c11282ce86c048ff9bd896440a27ea2431
hitaro2k <vlad.bojenko2005@gmail.com>
2023-05-20 16:07:03 +0300

    add popup

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -3,6 +3,71 @@
 var listItem = document.querySelectorAll(".list-item");
 var startBtn = document.querySelector(".hero__button");
 var readBtn = document.querySelector(".content__button");
+var arrowMore = document.querySelector(".learn-more__arrow");
+var loginBtn = document.getElementById("login-btn");
+var popup = document.querySelector(".popup");
+var popupLogin = document.querySelector(".popup__login");
+var popupRegister = document.querySelector(".popup__register");
+var fluencePartLogin = document.querySelector("#login-fluence");
+var brandPartLogin = document.querySelector("#login-brands");
+var fluencePartSign = document.querySelector("#sign-fluence");
+var brandPartSign = document.querySelector("#sign-brands");
+var heroContainer = document.getElementById("hero-container");
+var hero = document.querySelector(".hero");
+var journeyBtn = document.querySelector('.hero__button');
+var journey = document.querySelector(".tell-us");
+fluencePartLogin.addEventListener("click", function () {
+  fluencePartLogin.classList.add("active");
+  brandPartLogin.classList.remove("active");
+});
+brandPartLogin.addEventListener("click", function () {
+  brandPartLogin.classList.add("active");
+  fluencePartLogin.classList.remove("active");
+});
+fluencePartSign.addEventListener("click", function () {
+  fluencePartSign.classList.add("active");
+  brandPartSign.classList.remove("active");
+});
+brandPartSign.addEventListener("click", function () {
+  brandPartSign.classList.add("active");
+  fluencePartSign.classList.remove("active");
+});
+journeyBtn.addEventListener("click", function () {
+  popup.style.transform = "translateX(0%)";
+  journey.style.display = "flex";
+  popupLogin.style.display = "none";
+  popupRegister.style.display = "none";
+  heroContainer.style.display = "none";
+}); // ! LOGIN
+
+loginBtn.onmouseenter = function () {
+  loginBtn.style.transition = '2s';
+  loginBtn.style.transform = "scale(1.1)";
+};
+
+loginBtn.onmouseleave = function () {
+  loginBtn.style.transition = '2s';
+  loginBtn.style.transform = "scale(1)";
+};
+
+loginBtn.addEventListener("click", function () {
+  popup.style.transform = "translateX(0%)";
+  heroContainer.style.display = "none";
+  loginBtn.style.display = "none";
+  hero.style.paddingTop = 600 + "px";
+  journey.style.display = "none";
+}); // ! ARROW
+
+arrowMore.onmouseenter = function () {
+  arrowMore.style.transition = '2s';
+  arrowMore.style.transform = "scale(1.1)";
+};
+
+arrowMore.onmouseleave = function () {
+  arrowMore.style.transition = '2s';
+  arrowMore.style.transform = "scale(1)";
+};
+
 listItem.forEach(function (item) {
   item.onmouseenter = function () {
     item.style.color = "#9f41cc";
--- js/index.js
+++ js/index.js
@@ -1,9 +1,75 @@
 "use strict"
 
-let listItem = document.querySelectorAll(".list-item")
-let startBtn = document.querySelector(".hero__button")
-let readBtn = document.querySelector(".content__button")
+const listItem = document.querySelectorAll(".list-item")
+const startBtn = document.querySelector(".hero__button")
+const readBtn = document.querySelector(".content__button")
+const arrowMore = document.querySelector(".learn-more__arrow")
+const loginBtn = document.getElementById("login-btn")
+let popup = document.querySelector(".popup")
+let popupLogin = document.querySelector(".popup__login")
+let popupRegister = document.querySelector(".popup__register")
+let fluencePartLogin = document.querySelector("#login-fluence")
+let brandPartLogin = document.querySelector("#login-brands")
+let fluencePartSign = document.querySelector("#sign-fluence")
+let brandPartSign = document.querySelector("#sign-brands")
+let heroContainer = document.getElementById("hero-container")
+let hero = document.querySelector(".hero")
+let journeyBtn = document.querySelector('.hero__button')
+let journey = document.querySelector(".tell-us")
 
+fluencePartLogin.addEventListener("click" , ()=>{
+    fluencePartLogin.classList.add("active")
+    brandPartLogin.classList.remove("active") 
+})
+brandPartLogin.addEventListener("click" , ()=>{
+    brandPartLogin.classList.add("active")
+    fluencePartLogin.classList.remove("active")
+})
+
+fluencePartSign.addEventListener("click" , ()=>{
+    fluencePartSign.classList.add("active")
+    brandPartSign.classList.remove("active") 
+})
+brandPartSign.addEventListener("click" , ()=>{
+    brandPartSign.classList.add("active")
+    fluencePartSign.classList.remove("active")
+})
+
+journeyBtn.addEventListener("click" ,()=>{
+    popup.style.transform = "translateX(0%)"
+    journey.style.display = "flex"
+    popupLogin.style.display = "none"
+    popupRegister.style.display = "none"
+    heroContainer.style.display = "none"
+
+})
+
+// ! LOGIN
+loginBtn.onmouseenter = ()=>{
+    loginBtn.style.transition = '2s'
+    loginBtn.style.transform  = "scale(1.1)"
+}
+loginBtn.onmouseleave = ()=>{
+    loginBtn.style.transition = '2s'
+    loginBtn.style.transform  = "scale(1)"
+}
+loginBtn.addEventListener("click" , ()=>{
+    popup.style.transform = "translateX(0%)"
+    heroContainer.style.display = "none"
+    loginBtn.style.display = "none"
+    hero.style.paddingTop = 600 + "px"
+    journey.style.display = "none"
+})
+
+// ! ARROW
+arrowMore.onmouseenter = ()=>{
+    arrowMore.style.transition = '2s'
+    arrowMore.style.transform  = "scale(1.1)"
+}
+arrowMore.onmouseleave = ()=>{
+    arrowMore.style.transition = '2s'
+    arrowMore.style.transform  = "scale(1)"
+}
 listItem.forEach(item =>{
     item.onmouseenter = () =>{
         item.style.color = "#9f41cc"
